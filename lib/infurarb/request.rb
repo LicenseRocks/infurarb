e7ebb65c8a89323d3e2efd8f00369f992048082d
Adam Groves <adam.groves@gmail.com>
2018-11-06 11:01:54 +0100

    Added all JSON RPC methods

--- lib/infurarb/request.rb
+++ lib/infurarb/request.rb
@@ -1,9 +1,36 @@
 module Infurarb
   class Request
     RPC_METHODS = %i[
+      eth_accounts
       eth_blockNumber
+      eth_gasPrice
+      eth_getBalance
+      eth_getBlockByHash
+      eth_getBlockByNumber
       eth_getTransactionReceipt
+      eth_getBlockTransactionCountByHash
+      eth_getBlockTransactionCountByNumber
+      eth_getCode
       eth_getLogs
+      eth_getStorageAt
+      eth_getTransactionByBlockHashAndIndex
+      eth_getTransactionByBlockNumberAndIndex
+      eth_getUncleByBlockNumberAndIndex
+      eth_getUncleCountByBlockHash
+      eth_getUncleCountByBlockNumber
+      eth_getWork
+      eth_hashrate
+      eth_mining
+      eth_protocolVersion
+      eth_syncing
+      eth_getTransactionByHash
+      eth_getTransactionCount
+      web3_clientVersion
+      eth_call
+      eth_estimateGas
+      eth_sendRawTransaction
+      eth_submitHashrate
+      eth_submitWork
     ].freeze
 
     attr_reader :endpoint, :method_name, :params
@@ -14,11 +41,13 @@ module Infurarb
     end
 
     def call
-      response = HTTP.post(
-        endpoint,
-        json: { jsonrpc: '2.0', method: method_name, params: params, id: 1 }
+      Response.new(
+        HTTP.post(
+          endpoint,
+          json: { jsonrpc: '2.0', method: method_name, params: params, id: 1 }
+        ),
+        self
       )
-      Response.new(response, self)
     end
   end
 end
